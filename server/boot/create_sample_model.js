ea6173a436633e71d56219626eae63989d497baf
Matej Minárik <matej.minarik@agile-ict.com>
2016-04-04 10:20:03 +0200

    Add Doctor/Patient mapping and Status to Visit

--- server/boot/create_sample_model.js
+++ server/boot/create_sample_model.js
@@ -6,7 +6,11 @@ module.exports = function(app) {
     app.models.APIUser.create([
       {name: 'Pepik', pin: '898981/9897', phone: '54548888', address: 'Vancouver', role:'admin'},
       {name: 'Fera', pin: '8787878/9897', phone: '1111111', address: 'Kosice', role: 'doctor'},
-      {name: 'Mira', pin: '8788/9897', phone: '11111541', address: 'Brno', role: 'patient'}
+      {name: 'Jera', pin: '8787878/9898', phone: '1111112', address: 'Kosice', role: 'doctor'},
+      {name: 'Dera', pin: '8787878/9899', phone: '1111113', address: 'Kosice', role: 'doctor'},
+      {name: 'Mira', pin: '8788/9897', phone: '11111541', address: 'Brno', role: 'patient'},
+      {name: 'Kira', pin: '8788/9898', phone: '11111542', address: 'Brno', role: 'patient'},
+      {name: 'Tira', pin: '8788/9899', phone: '11111543', address: 'Brno', role: 'patient'}
     ], function(err, APIUsers) {
       if (err) throw err;
  
@@ -39,7 +43,7 @@ module.exports = function(app) {
     if (err) throw err;
  
     app.models.Visit.create([
-      {date: '2016-03-12', time: '12:00', APIuserId: '1'},
+      {date: '2016-03-12', time: '12:00', doctor_id: '1', patient_id: '5', status: 'new'},
     ], function(err, Visits) {
       if (err) throw err;
  
